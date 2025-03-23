b760593edf1ce506cba1fc3bc448052366c3d04f
Victor Frangov <59508395+vict0rfr@users.noreply.github.com>
2025-03-22 21:54:38 -0400

    v1

--- fib.c
+++ fib.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "esp_log.h"
+#include "freertos/task.h"
+#include <math.h>
 
-void app_main(void)
-{
+int fib(int n) {
+    double phi = (1 + sqrt(5)) / 2;
+    return round(pow(phi, n) / sqrt(5));
+}
 
+void app_main(void){
+    char* task = pcTaskGetName(NULL);
+    
+    for(int n = 0; n < 20; n++){
+        ESP_LOGI(task, "%d, %i\n", fib(n), n);
+    }
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
 }
